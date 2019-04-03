82237b5cdb18d405db614312aa28d49d687d59a7
vlad <v.pobereznichenko@publbox.com>
2019-04-03 13:29:42 +0300

    initial commit

---
+++ src/String1EndMatchingString2.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App;
+
+
+class String1EndMatchingString2
+{
+    /**
+     * @param string $str1
+     * @param string $str2
+     * @return bool
+     */
+    public function endingCheck(string $str1, string $str2)
+    {
+        $str2Len = strlen($str2);
+        $str1End = substr($str1, -$str2Len);
+
+        if ($str1End == $str2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/String1EndMatchingString2Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class String1EndMatchingString2Test extends TestCase
+{
+
+    public function testEndingCheck()
+    {
+        $stringTest = new String1EndMatchingString2();
+        self::assertTrue($stringTest->endingCheck('Samurai', 'ai'));
+    }
+}
