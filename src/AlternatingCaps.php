1d9fbdfb0bafa538350f0e014a4b94169c0a508c
vlad <v.pobereznichenko@publbox.com>
2019-04-03 15:58:32 +0300

    initial commit

---
+++ src/AlternatingCaps.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App;
+
+
+class AlternatingCaps
+{
+    /**
+     * @param string $str
+     * @return string
+     */
+    public function alternatingCaps(string $str)
+    {
+        $result = '';
+        $upper = true;
+        foreach (str_split(strtolower($str)) as $c) {
+            $result .= ($upper) ? strtoupper($c) : $c;
+            $upper = !$upper;
+        }
+
+        return $result;
+    }
+}
---
+++ src/AlternatingCapsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class AlternatingCapsTest extends TestCase
+{
+
+    public function testAlternatingCaps()
+    {
+        $testCase = new AlternatingCaps();
+        self::assertEquals('HeLlO', $testCase->alternatingCaps('Hello'));
+    }
+}
