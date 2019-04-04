faeec4a4aab27c0c70dde709e1799142aa314a63
vlad <v.pobereznichenko@publbox.com>
2019-04-04 17:40:59 +0300

    -Vectors added -Multiple UnitTest cases added

--- src/MapReducePatternTest.php
+++ src/MapReducePatternTest.php
@@ -11,5 +11,7 @@ class MapReducePatternTest extends TestCase
         $test = new MapReducePattern();
         self::assertEquals(5, $test->magnitude([3, 4]));
         self::assertEquals(10, $test->magnitude([0, 0, -10]));
+        self::assertEquals(0, $test->magnitude([]));
+        self::assertEquals(10.677078252031311, $test->magnitude([2, 3, 6, 1, 8]));
     }
 }
