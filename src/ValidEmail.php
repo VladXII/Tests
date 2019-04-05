285a4407426d36aa1523dbd7ac4260c1d8839b9a
vlad <v.pobereznichenko@publbox.com>
2019-04-05 10:46:26 +0300

    -ValidPhoneNumber alternative solution added

--- src/ValidEmail.php
+++ src/ValidEmail.php
@@ -13,8 +13,8 @@ class ValidEmail
         $strAll = explode('@', $str);
         if (count($strAll) == 2) {
             $strR = explode('.', $strAll[1]);
-            if (isset($strAll[0]) and isset($strR[0]) and isset($strR[1])) {
-                if (strlen($strAll[0]) > 1 and strlen($strR[0]) > 1 and strlen($strR[1]) > 1) {
+            if (isset($strAll[0]) && isset($strR[0]) && isset($strR[1])) {
+                if (strlen($strAll[0]) > 1 && strlen($strR[0]) > 1 && strlen($strR[1]) > 1) {
                     return true;
                 }
             }
--- src/ValidPhoneNumber.php
+++ src/ValidPhoneNumber.php
@@ -5,15 +5,25 @@ namespace App;
 class ValidPhoneNumber
 {
     /**
+     * використати регулярку https://www.tutorialrepublic.com/php-tutorial/php-regular-expressions.php
+     *
      * @param string $str
      * @return bool
      */
     public function validPhoneNumber(string $str)
     {
-        $strArr = str_split($str, 1);
-        if ($strArr[0] == '(' && $strArr[4] == ')' && $strArr[5] == ' ' && $strArr[9] == '-' && count(
-            $strArr
-        ) == 14) {
+        //Перший варіант
+        //$strArr = str_split($str, 1);
+        //if ($strArr[0] == '(' && $strArr[4] == ')' && $strArr[5] == ' ' && $strArr[9] == '-' && count(
+        //   $strArr
+        //) == 14) {
+        //  return true;
+        //} else {
+        //return false;
+        //}
+
+        //Варіант з регуляркою
+        if (preg_match('/^\(\d{3}\) \d{3}-\d{4}$/', $str)) {
             return true;
         } else {
             return false;
